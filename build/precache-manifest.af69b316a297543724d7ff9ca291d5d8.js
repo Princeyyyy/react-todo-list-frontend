67de0d9742b224bb02dab79af2179ad743b1a45d
Princeyyyy <princemumo80@gmail.com>
2024-04-08 04:57:40 +0300

    blews

---
+++ build/precache-manifest.af69b316a297543724d7ff9ca291d5d8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9c51854fc11fac95d63d7e6a10bbcaf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "879acbcb9dbe2237b577",
+    "url": "/static/css/main.f7e6a10e.chunk.css"
+  },
+  {
+    "revision": "b5dd6d1d7c227d98b819",
+    "url": "/static/js/2.fc714256.chunk.js"
+  },
+  {
+    "revision": "3c3833cc90e51ae822f01e296c687ebb",
+    "url": "/static/js/2.fc714256.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f57d022da71bd0270fc",
+    "url": "/static/js/3.ecf70b03.chunk.js"
+  },
+  {
+    "revision": "879acbcb9dbe2237b577",
+    "url": "/static/js/main.6c1a91d3.chunk.js"
+  },
+  {
+    "revision": "399afe2a9258d170cf93",
+    "url": "/static/js/runtime-main.298d1203.js"
+  }
+]);
